ead897479cfe15d02b0ece747737e1cedb23a7dc
conversodinHXL <conversodin66@gmial.com>
2014-07-03 14:07:33 +0800

    never use this script until I update.
        I wrote annotation with hashtable.I havetested the memory-using,it is too high to bear.I prepare to rewrite the module in future withmongoDB.
        So, NEVER use this scrpit until I finish database-version.

--- snpAnnotation.pl
+++ snpAnnotation.pl
@@ -1,5 +1,59 @@
 #!/usr/bin/perl
 
+=pod
+
+=head1 DESCRIPTION
+	
+	This script contains only one function.
+	It is very useless.Because I wrote annotation with hashtable.I have
+tested the memory-using,it is too high to bear.I prepare to rewrite the module in future with
+mongoDB.
+	So, NEVER use this scrpit until I finish database-version.
+	
+=head2 Main Function
+
+=over 1
+	
+=item C<snpAnnotation>
+we provide the example script to annotate snps with knowns vcf,such as dbsnp,1000 genomes,hapmap;
+	
+=back
+
+=head1 LICENSE
+
+=head1 AUTHOR:
+Hu XiaoLin	L<conversodin66@gmail.com>
+
+=head1 SEE ALSO
+
+=over 12
+
+=item C<NCBI dbSNP>
+	L<http://www.ncbi.nlm.nih.gov/SNP/>
+
+
+=item C<1000 genomes>
+	L<http://www.1000genomes.org/>
+	
+=item C<hapmap>
+	L<http://hapmap.ncbi.nlm.nih.gov/>
+	
+=back
+
+
+
+
+
+
+
+
+
+
+=cut 
+
+
+
+
 
 push (@INC,'pwd');
 
