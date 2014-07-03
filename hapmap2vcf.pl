709ffa48871cdb3eb35f9ffcf6f75d4052fc339a
conversodinHXL <conversodin66@gmial.com>
2014-07-03 14:03:02 +0800

    Add perldoc description of this module.
    Never use Annotation module until I update.

--- hapmap2vcf.pl
+++ hapmap2vcf.pl
@@ -1,5 +1,60 @@
 #!/usr/bin/perl
 
+=pod
+
+=head1 DESCRIPTION
+	
+	This script contains two function. the first is converting hapmap files to
+vcf(4.0) files. the second is annotation.
+	The second is very useless.Because I wrote annotation with hashtable.I have
+test the memory-using is to high.I prepare to rewrite the module in future with
+mongoDB.
+	So, NEVER use this scrpit until I finish database-version.
+	
+=head2 Main Function
+
+=over 1
+=item C<hapmap2vcf>
+we provide the example script to convert hapmap-format files to vcf 4.0 files;
+	
+=item C<snpAnnotation>
+we provide the example script to annotate snps with knowns vcf,such as dbsnp,1000 genomes,hapmap;
+	
+=back
+
+=head1 LICENSE
+
+=head1 AUTHOR:
+Hu XiaoLin	L<conversodin66@gmail.com>
+
+=head1 SEE ALSO
+
+=over 12
+
+=item C<NCBI dbSNP>
+	L<http://www.ncbi.nlm.nih.gov/SNP/>
+
+
+=item C<1000 genomes>
+	L<http://www.1000genomes.org/>
+	
+=item C<hapmap>
+	L<http://hapmap.ncbi.nlm.nih.gov/>
+	
+=back
+
+
+
+
+
+
+
+
+
+
+=cut 
+
+
 
  push (@INC,'pwd');
 
