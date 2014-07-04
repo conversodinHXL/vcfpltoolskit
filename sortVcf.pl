e479c38105e1740b0b144f4924449112c1e6458d
Hu Xiaolin <conversodin66@gmial.com>
2014-07-04 17:38:36 +0800

    Update sortVcf.pl

--- sortVcf.pl
+++ sortVcf.pl
@@ -1,5 +1,50 @@
 #/usr/bin/perl
 
+
+=pod
+
+=head1 DESCRIPTION
+	
+	This module is for sorting vcf files.In this version,
+We can only sort vcf files by chromsome ID(number first,
+alphabet second).
+
+
+
+=head1 LICENSE
+
+=head1 AUTHOR:
+Hu XiaoLin	L<conversodin66@gmail.com>
+
+=head1 SEE ALSO
+
+=over 12
+
+=item C<NCBI dbSNP>
+	L<http://www.ncbi.nlm.nih.gov/SNP/>
+
+
+=item C<1000 genomes>
+	L<http://www.1000genomes.org/>
+	
+=item C<hapmap>
+	L<http://hapmap.ncbi.nlm.nih.gov/>
+	
+=back
+
+
+
+
+
+
+
+
+
+
+=cut 
+
+
+
 push (@INC,'pwd');
 
 use Data::Dumper;
